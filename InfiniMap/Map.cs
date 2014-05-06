76f66e61a6c020e295ac756cd432faa1594f5648
LambdaSix <LambdaSix@users.noreply.github.com>
2014-05-06 15:28:07 +0100

    Add unloading support to Map
    Adds various options for unloading chunks, broadly divided into two
    categories: Expiration and Area and intersections between them.

--- InfiniMap/Map.cs
+++ InfiniMap/Map.cs
@@ -1,20 +1,55 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace InfiniMap
 {
+    /// <summary>
+    /// Provides support for attaching Expiration to objects 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class CacheEntry<T>
+    {
+        private DateTime _expiration;
+        private readonly object _lock = new object();
+
+        public T Value { get; private set; }
+
+        public DateTime Expiration
+        {
+            get { lock (_lock) { return _expiration; } }
+            private set { lock (_lock) { _expiration = value; } }
+        }
+
+        public CacheEntry(T value, DateTime expiration)
+        {
+            Expiration = expiration;
+            Value = value;
+        }
+
+        public void Refresh()
+        {
+            lock (_lock)
+            {
+                Expiration = DateTime.Now;
+            }
+        }
+    }
+
     public class Map
     {
-        public IDictionary<Tuple<int, int>, Chunk> Chunks;
+        public IDictionary<Tuple<int, int>, CacheEntry<Chunk>> Chunks;
 
-        private int _chunkWidth;
-        private int _chunkHeight;
+        private readonly int _chunkWidth;
+        private readonly int _chunkHeight;
 
         public Map(int chunkHeight, int chunkWidth)
         {
             _chunkHeight = chunkHeight;
             _chunkWidth = chunkWidth;
-            Chunks = new Dictionary<Tuple<int, int>, Chunk>(64);
+            Chunks = new Dictionary<Tuple<int, int>, CacheEntry<Chunk>>(64);
         }
 
         private IEnumerable<Tuple<int, int>> Distance(int startX, int startY, int range)
@@ -23,7 +58,8 @@ namespace InfiniMap
         }
 
         /// <summary>
-        /// Very simple garbage collection, frees all chunks within the given range.
+        /// Unload all chunks within a given area.
+        /// Ignores expiration, all chunks are unloaded; expired or not.
         /// </summary>
         /// <remarks>
         /// Frees up chunks in a square pattern. Given (0,0) and a range of 1, free:
@@ -42,6 +78,104 @@ namespace InfiniMap
             }
         }
 
+        /// <summary>
+        /// Unload everything except the defined area.
+        /// Ignores expiration, all chunks are unloaded, expired or not.
+        /// </summary>
+        /// <remarks>
+        /// Frees chunks with a square pattern being kept in memory. Given (1,1) and a range of 1:
+        /// (0,0) (0,1) (0,2)
+        /// (1,0) [1,1] [1,2]
+        /// (2,0) [2,1] [2,2]
+        /// (3,0) (3,1) (2,2)
+        /// 
+        /// [x,y] are kept in memory, (x,y) are discared from memory.
+        /// </remarks>
+        /// <param name="curX">Chunk X to start from</param>
+        /// <param name="curY">Chunk Y to start from</param>
+        /// <param name="range">Square distance to keep in memory</param>
+        public void UnloadAreaInverted(int curX, int curY, int range)
+        {
+            var localChunks = Distance(curX, curY, range).ToList();
+            // Clean out chunks outside of (x,y) -> (x+range, y+range)
+            foreach (var pair in Chunks.Where(pair => !localChunks.Contains(pair.Key)))
+            {
+                Chunks.Remove(pair.Key);
+            }
+        }
+
+        /// <summary>
+        /// Unload all expired blocks within a given area.
+        /// </summary>
+        /// <remarks>
+        /// Follows the same selection rules as <seealso cref="UnloadArea"/> except for
+        /// considering expiration of chunks.
+        /// </remarks>
+        /// <param name="expiryTime">Expiry time to use, any chunks last accessed before this point are unloaded</param>
+        /// <param name="curX">Chunk X to start from</param>
+        /// <param name="curY">Chunk Y to start from</param>
+        /// <param name="range">Square distance to unload</param>
+        public void UnloadExpiredArea(DateTime expiryTime, int curX, int curY, int range)
+        {
+            CacheEntry<Chunk> chunk = null;
+            foreach (var result in Distance(curX, curY, range).Select(pair => new {pair, found = Chunks.TryGetValue(pair, out chunk)}).Where(
+                    result => result.found).Where(result => chunk.Expiration <= expiryTime)) {
+                Chunks.Remove(result.pair);
+            }
+        }
+
+        /// <summary>
+        /// Unload all expired blocks outside of the defined area.
+        /// </summary>
+        /// <remarks>
+        /// Follows the same selection rules as <seealso cref="UnloadAreaInverted"/> except for
+        /// considering expiration of chunks.
+        /// </remarks>
+        /// <param name="expiryTime">Expiry time to use, any chunks last accessed before this point are unloaded</param>
+        /// <param name="curX">Chunk X to start from</param>
+        /// <param name="curY">Chunk Y to start form</param>
+        /// <param name="range">Square distance to unload</param>
+        public void UnloadedExpiredAreaInverted(DateTime expiryTime, int curX, int curY, int range)
+        {
+            CacheEntry<Chunk> chunk = null;
+            var localChunks = Distance(curX, curY, range).ToList();
+            // Clean out chunks outside of (x,y) -> (x+range,y+range) where Expiration <= expiryTime
+            foreach (var result in Chunks.Select(pair => new {key = pair.Key, found = Chunks.TryGetValue(pair.Key, out chunk)}).Where(
+                result => result.found).Where(pair => !localChunks.Contains(pair.key)).Where(result => chunk.Expiration <= expiryTime))
+            {
+                Chunks.Remove(result.key);
+            }
+        }
+        
+        /// <summary>
+        /// Unload all expired blocks.
+        /// This does not account for keeping any blocks in memory.
+        /// </summary>
+        /// <param name="expiryTime">Expiry time to use, any chunks last accessed before this point are unloaded</param>
+        public void UnloadExpired(DateTime expiryTime)
+        {
+            foreach (var pair in Chunks.Where(x => x.Value.Expiration <= expiryTime))
+            {
+                Chunks.Remove(pair.Key);
+            }
+        }
+
+        /// <summary>
+        /// Save a world into <paramref name="folderPath"/>
+        /// </summary>
+        /// <param name="folderPath">Root world folder to save into</param>
+        public void Write(string folderPath)
+        {
+        }
+
+        /// <summary>
+        /// Load a worldmap from <paramref name="folderPath"/>
+        /// </summary>
+        /// <param name="folderPath">Root world folder to load from</param>
+        public void Read(string folderPath)
+        {
+        }
+
         public Block this[int x, int y]
         {
             get
@@ -49,21 +183,22 @@ namespace InfiniMap
                 var xChunk = (int) Math.Floor(x/(float) _chunkHeight);
                 var yChunk = (int) Math.Floor(y/(float) _chunkWidth);
 
-                Chunk chunk;
+                CacheEntry<Chunk> chunk;
                 var foundChunk = Chunks.TryGetValue(Tuple.Create(xChunk, yChunk), out chunk);
                 if (foundChunk)
                 {
-                    return chunk[x, y];
+                    chunk.Refresh();
+                    return chunk.Value[x, y];
                 }
 
                 var newChunk = new Chunk(_chunkHeight, _chunkWidth);
-                Chunks.Add(Tuple.Create(xChunk, yChunk), newChunk);
+                Chunks.Add(Tuple.Create(xChunk, yChunk), new CacheEntry<Chunk>(newChunk, DateTime.Now));
                 return newChunk[x, y];
             }
             set
             {
                 // Block is a reference type, so we just discard a local pointer after
-                // alterting the object
+                // altering the object
                 var block = this[x, y];
                 block = value;
             }
