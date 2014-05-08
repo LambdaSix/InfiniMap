ac4622f49044ac847e41522a9877bb390673577e
LambdaSix <alexander.somerville.cox@gmail.com>
2014-05-08 12:28:25 +0100

    Update the unit tests to match new API

--- InfiniMap.Test/MapTests.cs
+++ InfiniMap.Test/MapTests.cs
@@ -1,4 +1,6 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace InfiniMap.Test
@@ -6,32 +8,111 @@ namespace InfiniMap.Test
     [TestFixture]
     public class MapTests
     {
-        private Map createMap(int chunkSize, int chunksToFill)
+        [Test]
+        public void CanInsertItems()
         {
-            var map = new Map(chunkSize, chunkSize);
+            // Insert structs
+            var mapStruct = new ChunkMap<StructItem>();
+            mapStruct[4, 4] = new StructItem() {ItemId = 4};
+            Assert.That(mapStruct[4, 4].ItemId == 4);
 
-            for (int x = 0; x <= (chunksToFill * chunkSize-1); x++)
-            {
-                for (int y = 0; y <= (chunksToFill * chunkSize-1); y++)
-                {
-                    map[x, y].BlockId = (ushort) x;
-                    map[x, y].BlockMeta = (ushort) y;
-                }
-            }
+            // Insert primitives
+            var mapPrim = new ChunkMap<float>();
+            mapPrim[4, 4] = 4.0f;
+            Assert.That(Math.Abs(mapPrim[4, 4] - 4.0f) < 0.001);
 
-            return map;
+            // Insert classes
+            var mapClass = new ChunkMap<ClassItem>();
+            mapClass[4, 4] = new ClassItem() {ItemId = 4};
+            Assert.That(mapClass[4, 4].ItemId == 4);
         }
 
         [Test]
-        public void FreesChunksProperly()
+        public void CanUseNegativePositions()
         {
-            var map = createMap(16, 1);
-
-            var dictionaryCount = map.Chunks.Count;
-
-            map.UnloadArea(0, 0, 1);
-
-            Assert.That(dictionaryCount > map.Chunks.Count);
+            var map = new ChunkMap<float>();
+            map[-4, -4] = 4.0f;
+            Assert.That(Math.Abs(map[-4, -4] - 4.0f) < 0.001);
         }
+
+        [Test]
+        public void CanEnumerateRanges()
+        {
+            var map = new ChunkMap<float>();
+            map[1, 2] = 4.0f;
+
+            Assert.That(map.Within(0, 0, 4, 4).Any());
+            Assert.That(map.Within(0, 0, 4, 4).Any(i => Math.Abs(i - 4.0f) < 0.001));
+        }
+
+        [Test]
+        public void CanAccessRandomly()
+        {
+            var map = new ChunkMap<float>();
+
+            map[-8, -8] = 4.0f;
+            map[-240, -778] = 8.0f;
+            map[8, 8] = 16.0f;
+            map[240, 778] = 32.0f;
+
+            Assert.That(Math.Abs(map[-240,-778] - 8) < 0.001);
+            Assert.That(Math.Abs(map[240, 778] - 32.0f) < 0.001);
+        }
+
+        [Test]
+        public void SparseCreation()
+        {
+            var map = new ChunkMap<float>();
+
+            map[-8, -8] = 4.0f;
+            map[-1024, -887] = 8.0f;
+
+            // With only two areas in memory, we only have (16*16)*2 blocks.
+            Assert.That(map.Count == 512);
+        }
+
+        [Test]
+        public void CanUseContains()
+        {
+            var map = new ChunkMap<float>();
+            map[1, 2] = 4.0f;
+
+            Assert.That(map.Contains(4.0f));
+            Assert.That(map.Contains(4.0f, new EqualityLambda<float>((a, b) => Math.Abs(a - b) < 0.001)));
+        }
+
+        #region Test Helpers
+
+        internal class EqualityLambda<T> : EqualityComparer<T>
+        {
+            private readonly Func<T, T, bool> _comparer;
+
+            public EqualityLambda(Func<T,T,bool> comparer)
+            {
+                _comparer = comparer;
+            }
+
+            public override bool Equals(T x, T y)
+            {
+                return _comparer(x, y);
+            }
+
+            public override int GetHashCode(T obj)
+            {
+                return obj.GetHashCode();
+            }
+        }
+
+        private struct StructItem
+        {
+            public int ItemId;
+        }
+
+        private class ClassItem
+        {
+            public int ItemId;
+        }
+
+        #endregion
     }
 }
