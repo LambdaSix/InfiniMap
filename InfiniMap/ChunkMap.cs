e28dac2af130c1326354daa82f0d33c11921f219
LambdaSix <LambdaSix@users.noreply.github.com>
2014-05-08 12:32:54 +0100

    Fix mistake on Within(...)'s loop logic
    Accidentally an infinite loop.

--- InfiniMap/ChunkMap.cs
+++ InfiniMap/ChunkMap.cs
@@ -40,9 +40,9 @@ namespace InfiniMap
 
         public IEnumerable<T> Within(int x0, int y0, int x1, int y1)
         {
-            for (int x = x0; x0 < x1; x++)
+            for (int x = x0; x < x1; x++)
             {
-                for (int y = y0; y0 < y1; y++)
+                for (int y = y0; y < y1; y++)
                 {
                     yield return this[x, y];
                 }
